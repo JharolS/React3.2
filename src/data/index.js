c891b77a44acbbf9bcb0f726d9d1db4f992e6948
unknown <sv73107535@idat.edu.pe>
2021-10-16 16:54:49 -0500

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,10 @@
+export const person=[
+    { 
+        src='',
+        name='Panji Sutrya',
+        state='Verificate',
+        money='12000',
+        transaction='3000',
+        rest='9000'
+    },
+]
